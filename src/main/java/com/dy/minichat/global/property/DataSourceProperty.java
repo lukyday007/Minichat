5389a4197d2b3298b2b1779faed5088caa7e016a
dayul <rainbow9517@naver.com>
2026-07-18 23:47:24 +0900

    docs: 주석 및 커넥션풀 로그 제거

--- src/main/java/com/dy/minichat/global/property/DataSourceProperty.java
+++ src/main/java/com/dy/minichat/global/property/DataSourceProperty.java
@@ -21,6 +21,7 @@ public class DataSourceProperty {
     private String password;
     private DatabaseInfo master;
     private List<DatabaseInfo> replicas;
+
     @ToString
     @Setter
     @Getter
@@ -28,25 +29,5 @@ public class DataSourceProperty {
     public static class DatabaseInfo {
         private String key;
         private String url;
-    }
-
-    @PostConstruct
-    void logOnStart() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("=== Loaded DataSourceProperty ===\n")
-                .append("  username=").append(username)
-                .append(", password=").append(password).append("\n")
-                .append("  master=").append(master.getKey())
-                .append(" (").append(master.getUrl()).append(")\n")
-                .append("  slaves=[");
-
-        if (replicas != null) {
-            sb.append(replicas.stream()
-                    .map(DatabaseInfo::getKey)
-                    .reduce((a, b) -> a + ", " + b)
-                    .orElse(""));
-        }
-        sb.append("]");
-        log.info(sb.toString());
     }
 }
